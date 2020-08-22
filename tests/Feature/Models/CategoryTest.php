f9beae1b78887e4bcb4a37be0263002fe0552445
Jorge <jorgeluizbsi@gmail.com>
2020-08-21 23:21:38 -0300

    executando test phpunit

---
+++ tests/Feature/Models/CategoryTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testList()
+    {
+        factory(Category::class, 1)->create();
+        $categories = Category::all();
+        $this->assertCount( 1, $categories);
+        $categoryKey = array_keys( $categories->first()->getAttributes() );
+
+        $this->assertEqualsCanonicalizing([
+            'id',
+            'name',
+            'description',
+            'is_active',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        ] , $categoryKey );
+    }
+
+    public function testCreate()
+    {
+        $category = Category::create([
+            'name' => 'test1'
+        ]);
+
+        $category->refresh();
+
+        $this->assertEquals('test1', $category->name );
+        $this->assertNull($category->description);
+        $this->assertTrue($category->is_active);
+
+        $category = Category::create([
+            'name' => 'test1',
+            'description' => null
+        ]);
+
+        $this->assertNull($category->description);
+
+
+        $category = Category::create([
+            'name' => 'test1',
+            'description' => 'test description'
+        ]);
+
+        $this->assertEquals('test description',$category->description);
+    }
+
+    public function testUpdate()
+    {
+        $category = factory( Category::class )->create([
+            'description' => 'test_description',
+            'is_active' => false
+        ])->first();
+
+        $data = [
+            'name' => 'test_update',
+            'description' => 'description_update',
+            'is_active' => true
+        ];
+
+        $category->update($data);
+
+        foreach($data as $key => $value){
+            $this->assertEquals($value,$category->{$key});
+        }
+    }
+}
---
+++ tests/Unit/Models/CategoryTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+
+    protected $category;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->category = new Category();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * This method is called after the last test of this test class is run.
+     */
+    public static function tearDownAfterClass(): void
+    {
+        parent::tearDownAfterClass();
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testFillableAtribute()
+    {
+        $fillable = ['name','description','is_active'];
+        $this->assertEquals($fillable, $this->category->getFillable());
+    }
+
+    public function testDatesAtribute()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        foreach($dates as $date) {
+            $this->assertContains($date, $this->category->getDates());
+        }
+
+        $this->assertCount(count($dates),$this->category->getDates());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [
+            SoftDeletes::class, Uuid::class
+        ];
+        $categoryTraits = array_keys(class_uses(Category::class));
+        $this->assertEquals($traits,$categoryTraits);
+    }
+
+    public function testCasts()
+    {
+        $casts = ['id' => 'string', 'is_active' => 'boolean'];
+        $this->assertEquals($casts,$this->category->getCasts());
+    }
+
+    public function testIncrementing()
+    {
+        $this->assertFalse($this->category->incrementing);
+    }
+}
