aee07864519f8877ec584efddb5768873b13674b
Jorge <jorgeluizbsi@gmail.com>
2020-08-27 16:32:06 -0300

    executando teste no category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -27,7 +27,9 @@ class CategoryController extends Controller
      */
     public function store(CategoryRequest $request)
     {
-        return Category::create($request->all());
+        $category = Category::create($request->all());
+        $category->refresh();
+        return $category;
     }
 
     /**
