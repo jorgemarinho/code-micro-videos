55be0f7039f22fa84507a1d50eda307fed934539
Jorge <jorgeluizbsi@gmail.com>
2021-04-26 19:49:13 -0300

    add criação build react no backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/{react?}', function ($react = null) {
+    return view('admin-frontend.index');
+})->where('react', '.*');
+
