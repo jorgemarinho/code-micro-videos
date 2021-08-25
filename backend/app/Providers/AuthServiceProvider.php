c5d41c27b1238a52eacd6bd16d31a2f5e4ae3536
Jorge <jorgeluizbsi@gmail.com>
2021-08-25 15:08:51 -0300

    autenticação com keycloak

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Auth\KeycloakGuard;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -26,5 +27,11 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        \Auth::extend('keycloak', function($app, $name, $config){
+            return new KeycloakGuard(
+                $app['tymon.jwt'],
+                $app['request']
+            );
+        });
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,11 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['namespace' => 'Api'], function () {
+Route::group(['namespace' => 'Api', 'middleware' => 'auth:api'], function () {
 
     $exceptionCreateEditAction = [
         'except' => ['create', 'edit']
